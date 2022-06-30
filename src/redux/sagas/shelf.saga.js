e2d80c70146ea1c6867e8eb10998178676fb6ee9
Badra Mohamed <moha1150@umn.edu>
2022-06-30 11:58:54 -0500

    shelfitems

--- src/redux/sagas/shelf.saga.js
+++ src/redux/sagas/shelf.saga.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
 // Saga will listen for 'FETCH_SHELF' action on page load
+
+
 function* fetchShelf() {
     console.log('in fetchShelf saga');
     try {
@@ -16,9 +18,25 @@ function* fetchShelf() {
 
     }
 }
+function* deleteShelf(item) {
+    try{
+     const res= yield axios({
+          method: 'DELETE', 
+          url: `api/shelf/${item.payload}`,
+    });
+        yield put({ type:'FETCH_SHELF'})
+    
+} 
+catch(err){
+    console.log('shelf delete request failed', error);
+}
+}
+
+
 
 function* shelfSaga() {
     yield takeLatest('FETCH_SHELF', fetchShelf);
+    yield takeLatest('DELETE_SHELF', deleteShelf);
 }
 
 export default shelfSaga;
