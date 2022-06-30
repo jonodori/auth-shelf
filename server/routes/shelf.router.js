b679cdc052a5d5373b6e9ff706c4ad0b6d3eb0cd
Jonathan Palalay <ring5pun@hotmail.com>
2022-06-30 11:44:19 -0500

    finished route for get

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware')
 
 /**
  * Get all of the items on the shelf
  */
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   res.sendStatus(200); // For testing only, can be removed
+
+  let queryText =
+    `SELECT * 
+    FROM "item"
+    `;
+
+    pool.query(queryText).then((result) => {
+      res.send(result.rows);
+  }).catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+  });
 });
+
 
 /**
  * Add an item for the logged in user to the shelf
